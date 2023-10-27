84ca818d9c09977e3cbc9c6cc6fe290ede015ae6
Aleksandr Cuchriajev <148259750+AlexCuchriajev@users.noreply.github.com>
2023-10-27 21:18:18 +0300

    add new directory conditions and loops,exercise#1

---
+++ 5-Exercises/6-Conditions-and-loops/loops-exercise-1.py
@@ -0,0 +1,8 @@
+## Loops. Exercise #1
+# We have a list of colors
+colors = ["red", "green", "yellow", "purple", "orange", "violet", "black"]
+# Write code that loops over the list and adds all the strings
+# together to form one large combined string
+# (don't add any spaces between them)
+# The combined string should be in all UPPERCASE as well
+# Save the result in a variable called result
