f446f56113a8b24a2a6d76e24812eb15e904a720
Aleksandr Cuchriajev <148259750+AlexCuchriajev@users.noreply.github.com>
2023-10-27 20:52:23 +0300

    add exercise #2 file with solution

---
+++ 5-Exercises/2-Lists/lists_exercise_2.py
@@ -0,0 +1,16 @@
+## Lists/ Exercise #2
+# We have a list of friends:
+friends = ["Kestas","Dovile","Alex", "Milana","Viktorija"]
+# Change "Kestas" to "Petras"
+# Change "Milana" to "Rasa"
+# Change "Viktorija" to "Sandra"
+# Hint: You can use the following syntax to change a value
+# of an existing list element at the specific index position:
+# my_list[0] = "NewValue"
+# Here, my_list would represent the variable holding the list
+# that we are modifying, and the 0 in square brackets would
+# be the index number of the targetted list element that we
+# are changing, and we would use the assignment operator = to set
+# it to a new value. You can use this approach with the people
+# list in this exercise to make the necessary modifications,
+# as instructed above!
---
+++ 5-Exercises/2-Lists/lists_exercise_2_solution.py
@@ -0,0 +1,16 @@
+## Lists/ Exercise #2
+# We have a list of friends:
+friends = ["Kestas","Dovile","Alex", "Milana","Viktorija"]
+# Change "Kestas" to "Petras"
+friends[0]="Petras"
+print(friends)
+# Change "Milana" to "Rasa"
+friends[-2]="Rasa"
+# or:
+friends[3]="Rasa"
+print(friends)
+# Change "Viktorija" to "Sandra"
+friends[-1]="Sandra"
+# or
+friends[4]="Sandra"
+print(friends)
