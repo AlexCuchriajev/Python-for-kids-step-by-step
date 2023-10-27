0b5555d0159d30467734bbe31179f15e56f5c2a2
Aleksandr Cuchriajev <148259750+AlexCuchriajev@users.noreply.github.com>
2023-10-27 21:42:02 +0300

    add exercise#3 file with solution

---
+++ 5-Exercises/2-Lists/list_exercise_3_solution.py
@@ -0,0 +1,18 @@
+## Lists. Exercise #3 solution
+# Create an empty list called friends
+friends=[]
+# Add the following friends to the friends list
+    # "Alex"
+    # "Sandra"
+    # "Max"
+# Use any of the list methods we've seen to accomplish this:
+# Solution with append() method
+friends.append("Alex")
+print(friends)
+friends.append("Sandra")
+print(friends)
+friends.append("Max")
+print(friends)
+# Solution with extend() method
+
+friends.extend(["Alex", "Sandra", "Max"])
---
+++ 5-Exercises/2-Lists/lists_exercise_3.py
@@ -0,0 +1,7 @@
+## Lists. Exercise #3
+# Create an empty list called friends
+# Add the following friends to the friends list
+    # "Alex"
+    # "Sandra"
+    # "Max"
+# Use any of the list methods we've seen to accomplish this:
