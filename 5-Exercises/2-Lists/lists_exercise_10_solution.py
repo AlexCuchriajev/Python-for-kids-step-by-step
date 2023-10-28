ed39ffea36d494787aaadb0a37aad9554af31e2c
Aleksandr Cuchriajev <148259750+AlexCuchriajev@users.noreply.github.com>
2023-10-28 22:39:18 +0300

    add exercise#10 file with solution

---
+++ 5-Exercises/2-Lists/lists_exercise_10_solution.py
@@ -0,0 +1,18 @@
+## Lists. Exercise #10
+# Using nested list comprehension and range() create a list "result"
+# with the following value :
+# [
+#  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+#  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+#  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+#  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+#  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+#  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+#  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+#  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+#  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+#  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+#  ]
+# It's a 10 x 10 nested list.  10 rows, each row contains' the numbers 0-9.
+result=[[k for k in range(0,10)] for i in range(0,10)]
+print(result)
