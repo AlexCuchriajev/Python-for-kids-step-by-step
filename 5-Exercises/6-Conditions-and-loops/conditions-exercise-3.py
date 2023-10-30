1c0ec87a02a6d8330b4f4b3b00db0073722e984e
Aleksandr Cuchriajev <148259750+AlexCuchriajev@users.noreply.github.com>
2023-10-30 18:44:39 +0200

    add conditions exercise #3 file and solution

---
+++ 5-Exercises/6-Conditions-and-loops/conditions-exercise-3.py
@@ -0,0 +1,8 @@
+## Conditions. Exercise #3
+import random
+choice = random.choice(['apple','salad', 'bacon', 'orange', 'sausage', 'tomato'])
+# The code randomly set the 'choice' variable to one from the list
+# You need to write code that will classify what food is.
+# If food is set to either 'apple' or 'orange', your code should print 'fruit'.
+# If food is set to either 'bacon' or 'sausage', your code should print 'meat'
+# If food is set to either 'salad' or 'tomato' your code should print 'vegetable'
