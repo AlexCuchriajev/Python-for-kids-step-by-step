27010e2feadd658bda6a07ab6ff859dcfb63beba
Aleksandr Cuchriajev <148259750+AlexCuchriajev@users.noreply.github.com>
2023-11-01 09:33:56 +0200

    add tuples exercise#1 file and solution

---
+++ 5-Exercises/5-Tuples/tuples-exercise-1-solution.py
@@ -0,0 +1,17 @@
+## Tuples. Exercise #1 solution:
+
+# Create a variable with name 'numbers' which is a tuple
+# with the values 1, 2, 3 and 4 inside.
+numbers=(1,2,3,4)
+print(numbers)
+
+# Create a variable with name 'number' which is a tuple
+# with the value 1 inside.
+number=(1,)
+print(number)
+
+# Create a variable with name 'constants' which is
+# the result of the 'numbers' variable converted to a tuple
+numbers = [10,20,30]
+constants=tuple(numbers)
+print(constants)
---
+++ 5-Exercises/5-Tuples/tuples-exercise-1.py
@@ -0,0 +1,11 @@
+## Tuples. Exercise #1
+
+# Create a variable with name 'numbers' which is a tuple
+# with the values 1, 2, 3 and 4 inside.
+
+# Create a variable with name 'number' which is a tuple
+# with the value 1 inside.
+
+# Create a variable with name 'constants' which is
+# the result of the 'numbers' variable converted to a tuple
+
