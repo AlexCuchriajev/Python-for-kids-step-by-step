94de8fdef3d54e599f70a79cd39a0bfc2389fbdf
Aleksandr Cuchriajev <148259750+AlexCuchriajev@users.noreply.github.com>
2023-11-01 17:26:11 +0200

    add exercise#12 file and solution

---
+++ 5-Exercises/7-Functions/functions-exercise-12.py
@@ -0,0 +1,11 @@
+## Functions. Exercise #12
+# Write a function with name 'count_single_letter'.
+# This function takes in two arguments (two strings).
+# The first argument should be a word and the second should be a letter.
+# The function returns the number of times that letter
+# appears in the word. If the letter is not in the word, the function should return 0.
+# Important: The input swould be not dependent on the case
+# (can be in lowercase or uppercase.)
+# Hint: use count() method
+def count_single_letter(word,letter):
+    pass
